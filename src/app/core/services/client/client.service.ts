05b279d81f84954bb9fe441e07680c70492189c2
sjimen13 <sjimen13@gmail.com>
2021-05-23 10:51:20 -0500

    step3-add-warranties

--- src/app/core/services/client/client.service.ts
+++ src/app/core/services/client/client.service.ts
@@ -4,6 +4,7 @@ import { environment } from 'src/environments/environment';
 
 import { ClientModel } from './../../models/client.model';
 import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -13,7 +14,7 @@ export class ClientService {
 
   constructor(private http: HttpClient) {}
 
-  createClient(client: ClientModel) {
+  createClient(client: ClientModel): Observable<any> {
     return this.http.post(`${this.URL}/clients`, client);
   }
 }
--- src/app/wizard-warranty/components/step3/step3.component.ts
+++ src/app/wizard-warranty/components/step3/step3.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { WarrantyTypeService } from './../../../core/services/warrantyType/warranty-type.service';
 import { WarrantyTypeModule } from './../../../core/models/warranty-type.model';
+import {
+  FormArray,
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators,
+} from '@angular/forms';
+import { element } from 'protractor';
 @Component({
   selector: 'app-step3',
   templateUrl: './step3.component.html',
@@ -11,13 +19,49 @@ export class Step3Component implements OnInit {
   @Output() stepChanged: EventEmitter<number> = new EventEmitter();
   warrantiesTypes: WarrantyTypeModule[];
 
-  constructor(private warrantyTypeService: WarrantyTypeService) {
-    this.warrantyTypeService
-      .getAll()
-      .subscribe((data) => (this.warrantiesTypes = data));
+  formStep3: FormGroup;
+
+  // postVentasArray = new FormControl();
+
+  constructor(
+    private warrantyTypeService: WarrantyTypeService,
+    private fb: FormBuilder
+  ) {
+    this.createForm();
+    this.warrantyTypeService.getAll().subscribe((data) => {
+      this.warrantiesTypes = data.map((el) => {
+        const newElement = { ...el, checked: false };
+        console.log(newElement);
+        return newElement;
+      });
+    });
   }
 
   ngOnInit(): void {}
+
+  createForm(): void {
+    this.formStep3 = this.fb.group({
+      warranties: this.fb.array([]),
+      explanation: ['', Validators.required],
+    });
+  }
+
+  get warrantiesArray(): FormArray {
+    return this.formStep3.get('warranties') as FormArray;
+  }
+
+  isChecked(item): void {
+    if (item.checked === false) {
+      this.warrantiesArray.push(this.fb.control(item));
+    } else {
+      const arr = this.warrantiesArray.value;
+      const arrId = arr.reduce(...arr, arr.id);
+      console.log(arrId);
+      const index = 1;
+      this.warrantiesArray.removeAt(index);
+    }
+    item.checked = !item.checked;
+  }
 
   stepMinus(): any {
     return this.stepChanged.emit(this.initStep - 1);
